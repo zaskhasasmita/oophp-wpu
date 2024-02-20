4dfaf72de530b17adf7a7bf4df3b6b8631495ce3
zaskhasasmita <zaskhasasmita22@gmail.com>
2024-02-20 19:57:57 +0700

    Feat : Add simple properties and methods

--- produk.php
+++ produk.php
@@ -1,11 +1,38 @@
 <?php
 
-// Jualan Produk
-// Komik
-// Game 
 class Produk {
+    public  $judul = "judul",
+            $penulis = "penulis",
+            $penerbit = "penerbit",
+            $harga = 0;
+
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
 
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Mobile Legend";
+// $produk2->tambahProperty = "hahaha";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+$produk4 = new Produk();
+$produk4->judul = "Dota 2";
+$produk4->penulis = "Lord Gaben";
+$produk4->penerbit = "Steam";
+$produk4->harga = 12000;
+
+echo "Komik : ". $produk3->getLabel();
+echo "<br>";
+echo "Game : ". $produk4->getLabel();
